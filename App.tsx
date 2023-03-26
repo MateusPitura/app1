7ef0b1fc9c8a27aa1770f9843cd19be3daa07c4a
Mateus Pitura <mateuspitura@gmail.com>
2023-03-26 09:49:45 -0300

    Adicionando arrow function

--- App.tsx
+++ App.tsx
@@ -3,10 +3,18 @@ import {StyleSheet, View, Text, Image} from 'react-native';
 import C2 from './componentes/Caixas';
 import Estilos from './estilos/style';
 
+const dobro=(n: number)=>n*2
+
+const soma = (n1: number,n2: number)=>{
+  var soma=n1+n2;
+  return soma;
+}
+
 export default function App1(){
   var vexibir=false;
   return(
     <View style={Estilos.conteiner}>
+      <Text>{dobro(2)}, {soma(2,4)}</Text>
       {/*<Image
        source={require('./assets/dog.png')}
         style={Estilos.logo}
@@ -17,7 +25,6 @@ export default function App1(){
         }}
         style={{width:250, height:250}}
       />
-      <C2 exibir={vexibir}/>
       <Text style={Estilos.textoTitulo}>Olá mundo</Text>
       <Text style={Estilos.textoPadrao}>Hello world</Text>
       {vexibir && <Text>Hello world</Text>}

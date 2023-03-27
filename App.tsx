9971be9ccad6ba84a96659d2299c7423c90c0720
Mateus Pitura <mateuspitura@gmail.com>
2023-03-27 06:53:41 -0300

    Adicionando botão

--- App.tsx
+++ App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import {StyleSheet, View, Text, Image} from 'react-native';
+import {StyleSheet, View, Text, Image, Button, Alert} from 'react-native';
 import C2 from './componentes/Caixas';
 import Estilos from './estilos/style';
 
-const dobro=(n: number)=>n*2
-
-const soma = (n1: number,n2: number)=>{
-  var soma=n1+n2;
-  return soma;
-}
+const msg=()=>{Alert.alert("Olá", "mundo")};
 
 export default function App1(){
   var vexibir=false;
   return(
     <View style={Estilos.conteiner}>
-      <Text>{dobro(2)}, {soma(2,4)}</Text>
-      {/*<Image
-       source={require('./assets/dog.png')}
-        style={Estilos.logo}
-      />*/}
+      <Button
+        title="Compre agora"
+        onPress={msg}
+      />
       <Image 
         source={{
           uri:'http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png'

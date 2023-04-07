1279ce776bd52403da37de29052792b05ee95bc2
Mateus Pitura <mateuspitura@gmail.com>
2023-04-07 06:54:00 -0300

    feat(project): adicionando refresh
    Quando puxado para baixo chama-se o evento refreshControl

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import React, {useState} from 'react';
 import {View, Text, Button, SafeAreaView} from 'react-native';
 import Estilos from './estilos/style';
-import Modal from './componentes/Modal'
+import Modal from './componentes/Modal';
+import Rl from './componentes/Rolagem';
 
 export default function App1(){
   return(
     <SafeAreaView style={Estilos.conteiner}>
         <Text style={Estilos.textoTitulo}>Olá mundo</Text>
         <Text style={Estilos.textoPadrao}>Hello world</Text>
-        <Modal/>
+        <Rl/>
     </SafeAreaView>
   );
 }; 
--- componentes/Rolagem.tsx
+++ componentes/Rolagem.tsx
@@ -1,10 +1,26 @@
 import React, {useState} from 'react'
-import {View, Text, ScrollView} from 'react-native'
+import {View, Text, ScrollView, RefreshControl} from 'react-native'
 
 export default function(){
+
+    const [atualizando, setAtualizando]=useState(false)
+
+    function aoAtualizar(){
+        setAtualizando(true);
+        setAtualizando(false);
+    }
+
     return(
          <View style={{flex:1}}>
-            <ScrollView style={{backgroundColor:'#bbb', padding:15}}>
+            <ScrollView 
+                style={{backgroundColor:'#bbb', padding:15}}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={atualizando}
+                        onRefresh={aoAtualizar}
+                    />
+                }
+            >
                 <Text style={{fontSize:30}}>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</Text>
             </ScrollView>
          </View>

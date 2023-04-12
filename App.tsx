adb0b19ad1d423542d040bba6b73588ff83686c7
Mateus Pitura <mateuspitura@gmail.com>
2023-04-12 06:53:55 -0300

    feat(project): adicionado telas com parâmetros

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import {NavigationContainer} from '@react-navigation/native';
 
 const Pilha=createStackNavigator();
 
-function TelaHome({navigation}){
+function TelaHome({navigation}: any){
   return(
     <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
       <Text>Tela Home</Text>
@@ -15,20 +15,56 @@ function TelaHome({navigation}){
         onPress={()=>navigation.navigate("Canal")}
       />
       <Button
-        title="Voltar"
-        onPress={()=>navigation.goBack()}
+        title="Tela Cursos"
+        onPress={()=>navigation.navigate("Cursos")}
       />
     </View>
   )
 }
 
-function TelaCanal({navigation}){
+function TelaCanal({navigation}: any){
   return(
     <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
       <Text>Tela Canal</Text>
       <Button
         title="Tela Home"
         onPress={()=>navigation.navigate("Home")}
+      />
+      <Button
+        title=""
+        onPress={()=>navigation.goBack()}
+      />
+    </View>
+  )
+}
+
+function TelaCursos({navigation}: any){
+  return(
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Text>Tela Cursos</Text>
+      <Button
+        title="React Native"
+        onPress={()=>navigation.navigate("ReactNative", {aulas:100, autor:'Bruno'})}
+      />
+    </View>
+  )
+}
+
+function TelaReactNative({route, navigation}: any){
+  const {aulas} = route.params;
+  const {autor} = route.params;
+  return(
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Text>Tela Cursos</Text>
+      <Text>Aulas: {aulas}</Text>
+      <Text>Autor: {autor}</Text>
+      <Button
+        title="Home"
+        onPress={()=>navigation.navigate("Home")}
+      />
+      <Button
+        title=""
+        onPress={()=>navigation.goBack()}
       />
     </View>
   )
@@ -37,7 +73,7 @@ function TelaCanal({navigation}){
 export default function App1(){
   return(
     <NavigationContainer>
-      <Pilha.Navigator initialRouteName="Canal">
+      <Pilha.Navigator>
         <Pilha.Screen
           name="Home"
           component={TelaHome}
@@ -47,6 +83,16 @@ export default function App1(){
           name="Canal"
           component={TelaCanal}
           options={{title:'Tela Canal'}}
+        />
+        <Pilha.Screen
+          name="Cursos"
+          component={TelaCursos}
+          options={{title:'Tela Cursos'}}
+        />
+        <Pilha.Screen
+          name="ReactNative"
+          component={TelaReactNative}
+          options={{title:'Tela React Native'}}
         />
       </Pilha.Navigator>
     </NavigationContainer>

ebd8da0de80f0d95f5950152252acb59393f65e1
Mateus Pitura <mateuspitura@gmail.com>
2023-04-14 06:55:27 -0300

    feat(project): adicionado tab navigation

--- App.tsx
+++ App.tsx
@@ -2,23 +2,15 @@ import React from 'react';
 import {View, Text, SafeAreaView, Button, Alert} from 'react-native';
 import Estilos from './estilos/style';
 import Cabecalho from './estilos/cabecalho-style';
-import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-const Pilha=createStackNavigator();
+const Guias=createBottomTabNavigator();
 
 function TelaHome({navigation}: any){
   return(
     <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
       <Text>Tela Home</Text>
-      <Button
-        title="Tela Canal"
-        onPress={()=>navigation.navigate("Canal")}
-      />
-      <Button
-        title="Tela Cursos"
-        onPress={()=>navigation.navigate("Cursos")}
-      />
     </View>
   )
 }
@@ -27,14 +19,6 @@ function TelaCanal({navigation}: any){
   return(
     <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
       <Text>Tela Canal</Text>
-      <Button
-        title="Tela Home"
-        onPress={()=>navigation.navigate("Home")}
-      />
-      <Button
-        title=""
-        onPress={()=>navigation.goBack()}
-      />
     </View>
   )
 }
@@ -43,30 +27,6 @@ function TelaCursos({navigation}: any){
   return(
     <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
       <Text>Tela Cursos</Text>
-      <Button
-        title="React Native"
-        onPress={()=>navigation.navigate("ReactNative", {aulas:100, autor:'Bruno'})}
-      />
-    </View>
-  )
-}
-
-function TelaReactNative({route, navigation}: any){
-  const {aulas} = route.params;
-  const {autor} = route.params;
-  return(
-    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-      <Text>Tela Cursos</Text>
-      <Text>Aulas: {aulas}</Text>
-      <Text>Autor: {autor}</Text>
-      <Button
-        title="Home"
-        onPress={()=>navigation.navigate("Home")}
-      />
-      <Button
-        title=""
-        onPress={()=>navigation.goBack()}
-      />
     </View>
   )
 }
@@ -74,8 +34,8 @@ function TelaReactNative({route, navigation}: any){
 export default function App1(){
   return(
     <NavigationContainer>
-      <Pilha.Navigator>
-        <Pilha.Screen
+      <Guias.Navigator initialRouteName="Canal">
+        <Guias.Screen
           name="Home"
           component={TelaHome}
           options={{
@@ -96,31 +56,17 @@ export default function App1(){
             ),
           }}
         />
-        <Pilha.Screen
+        <Guias.Screen
           name="Canal"
           component={TelaCanal}
           options={{title:'Tela Canal'}}
         />
-        <Pilha.Screen
+        <Guias.Screen
           name="Cursos"
           component={TelaCursos}
           options={{title:'Tela Cursos'}}
         />
-        <Pilha.Screen
-          name="ReactNative"
-          component={TelaReactNative}
-          options={{
-            title:'Tela React Native',
-            headerStyle:{
-              backgroundColor:'#008',
-            },
-            headerTintColor:'#fff',
-            headerTitleStyle:{
-              fontWeight:'bold',
-            }
-          }}
-        />
-      </Pilha.Navigator>
+      </Guias.Navigator>
     </NavigationContainer>
   );
 }; 

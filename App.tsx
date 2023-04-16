1fdda55b58e4746a4b5c917a1b7f9c58a0d43729
Mateus Pitura <mateuspitura@gmail.com>
2023-04-16 06:54:26 -0300

    test(project): adicionado variáveis globais

--- App.tsx
+++ App.tsx
@@ -1,70 +1,20 @@
-import React from 'react';
-import {View, Text, SafeAreaView, Button, Alert} from 'react-native';
-import Estilos from './estilos/style';
-import {NavigationContainer} from '@react-navigation/native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import TelaHomeExterna from './componentes/TelaHome';
+import React, { Component } from 'react';
+import {View, Text, Button} from 'react-native';
+import Globais from './componentes/Globais';
 
-const Gavetas=createDrawerNavigator();
+export default class App1 extends Component{
 
-function TelaHome({navigation}: any){
-  return(
-    <TelaHomeExterna nav={navigation}/>
-  )
+  nome=Globais.nome;
+  canal = Globais.canal;
+  numero = Globais.num;
+
+  render(){
+    return(
+      <View>
+        <Text>Hello world {this.nome}</Text>
+        <Text>Hello world {this.canal}</Text>
+        <Text>Hello world {this.numero}</Text>
+      </View>
+    )
+  }
 }
-
-function TelaCanal({navigation}: any){
-  return(
-    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-      <Text>Tela Canal</Text>
-    </View>
-  )
-}
-
-function TelaCursos({navigation}: any){
-  return(
-    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-      <Text>Tela Cursos</Text>
-    </View>
-  )
-}
-
-export default function App1(){
-  return(
-    <NavigationContainer>
-      <Gavetas.Navigator initialRouteName="Canal">
-        <Gavetas.Screen
-          name="Home"
-          component={TelaHome}
-          options={{
-            title:'Tela Inicial',
-            headerStyle:{
-              backgroundColor:'#008',
-            },
-            headerTintColor:'#fff',
-            headerTitleStyle:{
-              fontWeight:'bold',
-            },
-            headerRight:()=>(
-              <Button
-                title="Cursos"
-                color="#000"
-                onPress={()=>Alert.alert('Botão Clicado')}
-              /> 
-            ),
-          }}
-        />
-        <Gavetas.Screen
-          name="Canal"
-          component={TelaCanal}
-          options={{title:'Tela Canal'}}
-        />
-        <Gavetas.Screen
-          name="Cursos"
-          component={TelaCursos}
-          options={{title:'Tela Cursos'}}
-        />
-      </Gavetas.Navigator>
-    </NavigationContainer>
-  );
-}; 
---
+++ componentes/Globais.tsx
@@ -0,0 +1,11 @@
+class Globais{
+    static nome = "Mateus"
+    static canal = "FelipeNeto"
+    static num = 2020
+
+    public setNome(nome: any){
+        nome=nome;
+    }
+}
+
+export default Globais
